fb36f38ff1f9ae223f0b4562ee4c9df19dd2297e
Mazen Ali <mazen.ali@itwm.fraunhofer.de>
2023-04-20 15:30:01 +0200

    Add and test datagen fat

--- qml_mor/datagen.py
+++ qml_mor/datagen.py
@@ -1,9 +1,12 @@
-from typing import Optional, TypeAlias, TypeVar, Generic, Tuple, Dict
+from typing import Optional, TypeVar, Generic, Tuple, Dict, Set, Union
 from abc import ABC, abstractmethod
 import torch
+import numpy as np
+from itertools import product
+from scipy.stats import qmc
 
-Tensor: TypeAlias = torch.Tensor
-Device: TypeAlias = torch.device
+Tensor = Union[torch.Tensor, np.ndarray]
+Device = torch.device
 D = TypeVar("D")
 
 
@@ -21,7 +24,6 @@ class DataGenTorch(ABC, Generic[D]):
     def __init__(
         self, seed: Optional[int] = None, device: Device = torch.device("cpu")
     ) -> None:
-
         self.__seed = seed
 
         if self.__seed is not None:
@@ -81,7 +83,6 @@ class DataGenCapacity(DataGenTorch[Dict[str, Tensor]]):
     def __init__(
         self, sizex: int, scale: float = 2.0, shift: float = -1.0, *args, **kwargs
     ) -> None:
-
         super().__init__(*args, **kwargs)
 
         self.sizex = sizex
@@ -111,6 +112,65 @@ class DataGenCapacity(DataGenTorch[Dict[str, Tensor]]):
         )
 
         data = {"X": X, "Y": Y}
+
+        return data
+
+
+class DataGenFat(DataGenTorch[Dict[str, Tensor]]):
+    """
+    Generates data for estimating fat shattering dimension.
+
+    Args:
+        sizex (int): The size of the input data (dim of feature space).
+        gamma (float): The fat shattering parameter gamma.
+            Defaults to 0.0 (pseudo-dimension).
+        scale (float, optional): The re-scaling factor for uniform
+            random numbers in [0, 1]. Defaults to 2.0.
+        shift (float, optional): The shift value for uniform
+            random numbers in [0, 1]. Defaults to -1.0.
+        args: Variable length argument list passed to the base class.
+        kwargs: Arbitrary keyword arguments passed to the base class.
+    """
+
+    def __init__(
+        self,
+        sizex: int,
+        gamma: float = 0.0,
+        scale: float = 2.0,
+        shift: float = -1.0,
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.sizex = sizex
+        self.gamma = gamma
+        self.scale = scale
+        self.shift = shift
+
+    def gen_data(self, d: int, Sb: int = 10, Sr: int = 10) -> Dict[str, Tensor]:
+        """
+        Generate the data.
+
+        Args:
+            d (int): The number of inputs to shatter.
+            Sb (int): The number of binary samples to check shattering.
+                Defaults to 10.
+            Sr (int): The number of level offset samples to check shattering.
+                Defaults to 10.
+
+        Returns:
+            data (Dict[str, Tensor]): A dictionary containing the generated data.
+        """
+
+        X = gen_synthetic_features(
+            d, self.sizex, self.seed, self.scale, self.shift, self.device
+        )
+        b = generate_samples_b_fat(d, Sb)
+        r = generate_samples_r_fat(d, Sr)
+        Y = gen_synthetic_labels_fat(b, r, self.gamma, self.device)
+
+        data = {"X": X, "Y": Y, "b": b, "r": r}
 
         return data
 
@@ -164,3 +224,137 @@ def gen_dataset_capacity(
     )
 
     return x, y
+
+
+def generate_samples_b_fat(d: int, S: int) -> np.ndarray:
+    """
+    Generate S unique samples of b from {0, 1}^d.
+
+    Args:
+        d (int): Number of input data samples for shattering.
+        S (int): Number of binary samples to check shattering
+            (for scalability if d is large).
+
+    Returns:
+        numpy.ndarray: An array of shape (S, d) containing unique samples of b.
+    """
+
+    max_values = 2**d
+    if S >= max_values:
+        possible_values = list(product([0, 1], repeat=d))
+        return np.array(possible_values)
+    else:
+        samples: Set[Tuple[int, ...]] = set()
+        while len(samples) < S:
+            b_sample = tuple(np.random.randint(0, 2, size=d))
+            samples.add(b_sample)
+        return np.array(list(samples))
+
+
+def generate_samples_r_fat(d: int, S: int) -> np.ndarray:
+    """
+    Generate S samples of r from [0, 1]^d using Latin Hypercube Sampling.
+
+    Args:
+        d (int): Dimension of the feature space.
+        S (int): Number of samples to generate.
+
+    Returns:
+        numpy.ndarray: An array of shape (S, d) containing samples of r.
+    """
+
+    sampler = qmc.LatinHypercube(d=d)
+    r_samples = sampler.random(n=S)
+    return r_samples
+
+
+def gen_synthetic_features(
+    d: int,
+    sizex: int,
+    seed: Optional[int] = None,
+    scale: float = 2.0,
+    shift: float = -1.0,
+    device: Device = torch.device("cpu"),
+) -> Tensor:
+    """
+    Generates d inputs x of dimension sizex sampled uniformly from scale*[0,1]+shift.
+
+    Args:
+        d (int): The number of inputs x to generate.
+        sizex (int): The size of each input.
+        seed (int, optional): The random seed to use for generating the features.
+            Defaults to None.
+        scale (float, optional): The re-scaling factor for uniform random numbers
+            in [0,1]. Defaults to 2.0.
+        shift (float, optional): The shift value for uniform random numbers [0,1].
+            Defaults to -1.0.
+        device (Device, optional): Torch device to run on. Defaults to CPU.
+
+    Returns:
+        Tensor: Tensor X of shape (d, sizex).
+    """
+
+    scale = 2.0
+    shift = -1.0
+
+    if seed is not None:
+        torch.manual_seed(seed)
+
+    x = (
+        scale
+        * torch.rand(d, sizex, dtype=torch.float64, device=device, requires_grad=False)
+        + shift
+    )
+
+    return x
+
+
+def gen_synthetic_labels_fat(
+    b: np.ndarray,
+    r: np.ndarray,
+    gamma: float = 0.0,
+    device: Device = torch.device("cpu"),
+) -> Tensor:
+    """
+    Generate constant label values equal to r_i + gamma
+    when b_i = 1 and r_i - gamma when b_i = 0.
+
+    Args:
+        b (numpy.ndarray): An array of shape (Sb,d)
+            containing Sb samples of d-dim binary values.
+        r (numpy.ndarray): An array of shape (Sr,d)
+            containing Sr samples of d-dim real values in [0, 1].
+        gamma (float, optional): The fat-shattering margin value.
+            Defaults to 0.0.
+        device (Device, optional): Torch device to run on. Defaults to CPU.
+
+    Returns:
+        Tensor: Y of shape (Sr, Sb, d).
+
+    Raises:
+        ValueError: If the length of b[0] is not the same as the length of r[0].
+    """
+
+    Sb = len(b)
+    Sr = len(r)
+    d = d1 = len(b[0])
+    d2 = len(r[0])
+
+    if d1 != d2:
+        raise ValueError(
+            f"The length of b[0] and r[0] are {d1} and {d2}. "
+            f"Should be constant and the same."
+        )
+
+    labels = np.zeros((Sr, Sb, d))
+    for sr in range(Sr):
+        for sb in range(Sb):
+            for i in range(d):
+                if b[sb][i] == 1:
+                    labels[sr, sb, i] = r[sr, i] + gamma
+                else:
+                    labels[sr, sb, i] = r[sr, i] - gamma
+
+    y = torch.tensor(labels, dtype=torch.float64, device=device, requires_grad=False)
+
+    return y
--- tests/test_datagen.py
+++ tests/test_datagen.py
@@ -1,6 +1,7 @@
 import unittest
 import torch
-from qml_mor.datagen import DataGenCapacity
+import numpy as np
+from qml_mor.datagen import DataGenCapacity, DataGenFat
 
 
 class TestDataGenCapacity(unittest.TestCase):
@@ -20,6 +21,31 @@ class TestDataGenCapacity(unittest.TestCase):
         self.assertIsInstance(y, torch.Tensor)
         self.assertEqual(x.shape, (N, sizex))
         self.assertEqual(y.shape, (num_samples, N))
+
+
+class TestDataGenFat(unittest.TestCase):
+    def test_gen_dataset(self):
+        d = 3
+        Sb = 5
+        Sr = 2
+        sizex = 2
+        gamma = 0.5
+        seed = 0
+
+        datagen = DataGenFat(sizex=sizex, gamma=gamma, seed=seed)
+
+        data = datagen.gen_data(d, Sb, Sr)
+        X = data["X"]
+        Y = data["Y"]
+        b = data["b"]
+        r = data["r"]
+
+        self.assertIsInstance(X, torch.Tensor)
+        self.assertIsInstance(Y, torch.Tensor)
+        self.assertIsInstance(b, np.ndarray)
+        self.assertIsInstance(r, np.ndarray)
+        self.assertEqual(X.shape, (d, sizex))
+        self.assertEqual(Y.shape, (Sr, Sb, d))
 
 
 if __name__ == "__main__":
